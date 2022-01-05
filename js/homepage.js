e2cb0be9a767cd207fd82199d970d3859aef732d
jessica14angulo <ang21016@byui.edu>
2022-01-05 11:33:23 -0400

    queryselector added

--- js/homepage.js
+++ js/homepage.js
@@ -1,2 +1,9 @@
-const options = {month: "numeric", day:"numeric", year: "numeric", hour:"2-digit", minute:"2-digit", second:"2-digit"};
+//current year
+document.addEventListener("DOMContentLoaded", function() { 
+    const yrSpan = document.querySelector('#currentyear');
+    const currentYr = new Date().getFullYear();
+    yrSpan.textContent = currentYr;
+  });
+//last modified date and hour
+const options = {month: "numeric", day:"numeric", year: "numeric", hour12: false, hour:"2-digit", minute:"2-digit", second:"2-digit"};
 document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US",options);
