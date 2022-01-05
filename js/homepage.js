fe6762b47a1f57915cb4f4e2a2c1e0bdcf61f1b9
jessica14angulo <ang21016@byui.edu>
2022-01-05 10:12:17 -0400

    styling

--- js/homepage.js
+++ js/homepage.js
@@ -1,2 +1,2 @@
-var modified = document.lastModified;
-alert(document.lastModified);
+const options = {month: "numeric", day:"numeric", year: "numeric", hour:"2-digit", minute:"2-digit", second:"2-digit"};
+document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US",options);
